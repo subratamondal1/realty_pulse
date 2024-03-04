d47972ebf84bb4597b5996fe8ddb70558399f0bd
Subrata Mondal <subratasubha2@gmail.com>
2024-03-04 13:46:32 +0530

    Convert Server Components to Client Components and Hooks: useRouter,useParams,useSearchParams,usePathname.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,29 @@
+"use client"; // Converts Server Components to Client
 import React from "react";
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
 
 const PropertyPage = () => {
-  return <div>Property Page</div>;
+  const router = useRouter();
+  const { id } = useParams();
+  const searchParam = useSearchParams();
+  const name = searchParam.get("name");
+  const pathname = usePathname();
+  console.log("Hello");
+  return (
+    <div>
+      <h1>Property Page</h1>
+      <button
+        onClick={() => router.push("/")}
+        className="bg-cyan-400 rounded-full px-4 text-xl font-bold">
+        Go to Home {pathname}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
